6ba52288d0f6720e7d611347ef0294bd9a24df51
rejuvyesh <rejuvyesh@gmail.com>
2012-12-20 17:17:48 +0530

    Some debugging option

--- mpdzen/downloadcovers.sh
+++ mpdzen/downloadcovers.sh
@@ -3,12 +3,18 @@
 # author: rejuvyesh
 # Desc. : Script to fetch cover for the current playing song from last fm
 
-mpdst=( ${(f)"$(mpc -h ${1:-localhost} status -f "%artist%\n%album%\n%title%")"} )
+mpdst=( ${(f)"$(mpc -h localhost status -f "%artist%\n%album%\n%title%")"} )
 ARTIST=$mpdst[1]
 ALBUM=$mpdst[2]
 SONG=$mpdst[3]
 
 quote=$'\042'
-cd ~/.covers
-cover_fetcher ${ARTIST} ${ALBUM} &> /dev/null
 
+if [[ "$1" = -d ]]; then
+    #debug
+    cd ~/.covers
+    cover_fetcher ${ARTIST} ${ALBUM}
+else
+    cd ~/.covers
+    cover_fetcher ${ARTIST} ${ALBUM} &> /dev/null
+fi
