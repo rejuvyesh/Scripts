cf8cb28cfbe52412a6b1850416717a5064981ca2
rejuvyesh <mail@rejuvyesh.com>
2014-03-30 11:51:49 +0530

    create mutt alias

---
+++ createalias.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+#
+# File: createalias.sh
+#
+# Created: Sunday, March 30 2014 by rejuvyesh <mail@rejuvyesh.com>
+# License: GNU GPL 3 <http://www.gnu.org/copyleft/gpl.html>
+#
+
+#!/bin/sh
+
+MESSAGE=$(cat)
+
+NEWALIAS=$(echo "${MESSAGE}" | grep ^"From: " | sed s/[\,\"\']//g | awk '{$1=""; if (NF == 3) {print "alias" $0;} else if (NF == 2) {print "alias" $0 $0;} else if (NF > 3) {print "alias", tolower($(NF-1))"-"tolower($2) $0;}}')
+
+if grep -Fxq "$NEWALIAS" $HOME/.mutt/aliases; then
+    :
+else
+    echo "$NEWALIAS" >> $HOME/.mutt/aliases
+fi
+
+echo "${MESSAGE}"
